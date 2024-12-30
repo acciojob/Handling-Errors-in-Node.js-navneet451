b02b356a07ffd87ee10f6d30bc2086d258966647
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-30 09:52:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  if(fs.existsSync(filePath)){
+    fs.readFile(filePath,"utf-8",(err, data)=>{
+      if(err){
+        console.log(err);
+      }
+      console.log(data);
+    })
+  } else{
+    console.log( "Column '${columnName}' not found in the CSV.");
+  }
 }
 
 // TODO: Call printFileContents with the command-line argument
+printFileContents("./output.txt");
